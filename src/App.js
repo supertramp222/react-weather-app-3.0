6c919b8eaa4140fd9ff0ee4a69fe06270098675b
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-08-16 19:44:32 +0200

    Deleted unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App</h1>
+      <footer>
+        This code is mad by Katharina Ramer and is open-sourced. View it on{" "}
+        <a href="" target="_blank">
+          GitHub.
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
