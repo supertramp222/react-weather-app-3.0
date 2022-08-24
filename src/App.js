6745c6e8704763d1194ee7e765c6d9f74348d2e9
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-08-24 16:44:24 +0200

    Added API

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This code is mad by Katharina Ramer and is open-sourced. View it on{" "}
           <a
             href="https://github.com/supertramp222/react-weather-app-3.0"
             target="_blank"
+            rel="noreferrer"
           >
             GitHub.
           </a>
