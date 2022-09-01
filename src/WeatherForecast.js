1cec30bb5f22aa73a6d65769921b45f368e90917
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-09-01 10:57:03 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
@@ -6,6 +6,11 @@ import "./WeatherForecast.css";
 export default function WeatherForecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(repsonse) {
     console.log(repsonse.data);
     setForecast(repsonse.data.daily);
@@ -18,7 +23,7 @@ export default function WeatherForecast(props) {
         <div className="WeatherForecast">
           <div className="row">
             {forecast.map(function (dailyForecast, index) {
-              if (index < 5) {
+              if (index > 0 && index < 7) {
                 return (
                   <div className="col" key={index}>
                     <WeatherForecastDay data={dailyForecast} />
