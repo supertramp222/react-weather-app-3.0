c3effae0b86df379d725b1cf6515e519e8efe6eb
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-09-01 09:23:07 +0200

    Fixed some mistakes in Weather Forecast Component

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -12,7 +12,7 @@ export default function WeatherForecastDay(props) {
   }
 
   function day() {
-    let date = new Date(props.date.dt * 1000);
+    let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -22,7 +22,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div className="WeatherForecastDay">
       <div className="WeatherForecast-date">{day()}</div>
-      <WeatherIcon code={props.data.icon} size={36} />
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
